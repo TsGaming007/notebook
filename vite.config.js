39d376cd2e0e0b53f126028f9fdeaaef3c74e976
TsGaming007 <tasosgats51@gmail.com>
2026-06-02 23:05:42 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,1 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/husserl-mereology/'
-})
+import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({plugins:[react()]});
